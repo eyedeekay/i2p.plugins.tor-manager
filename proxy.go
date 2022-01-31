edb4ce167051018350fc868fac4d0aa8b9d67444
idk <hankhill19580@gmail.com>
2022-01-31 16:24:15 -0500

    If a router isn't started, start one. If an HTTP proxy isn't started, start one.

--- proxy.go
+++ proxy.go
@@ -38,8 +38,6 @@ var (
 	runQuiet             = false
 )
 
-var addr string
-
 func proxy() {
 	ln, err := net.Listen("tcp", "127.0.0.1:4444")
 	if err != nil {
@@ -104,6 +102,9 @@ func proxyMain(ctx context.Context, ln net.Listener, cln net.Listener) {
 		Addr:              cln.Addr().String(),
 	}
 	ctrlsrv.Handler, err = i2phttpproxy.NewSAMHTTPController(profiles, srv)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
