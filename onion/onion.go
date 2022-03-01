40a306d370ba71c8c3e340ff6ade1ae54b6ca6a2
idk <hankhill19580@gmail.com>
2022-02-28 22:46:49 -0500

    check in onion service capability

--- onion/onion.go
+++ onion/onion.go
@@ -2,6 +2,8 @@ package i2pdotonion
 
 import (
 	"context"
+	"crypto"
+	"crypto/ed25519"
 	"embed"
 	"fmt"
 	"io/fs"
@@ -23,6 +25,7 @@ var content embed.FS
 type I2POnionService struct {
 	OnionService net.Listener
 	ServeDir     string
+	Keys         crypto.PrivateKey
 }
 
 func NewOnionService(dir string) (*I2POnionService, error) {
@@ -32,7 +35,49 @@ func NewOnionService(dir string) (*I2POnionService, error) {
 	if err := ios.UnpackSite(); err != nil {
 		return nil, err
 	}
+
+	if file, err := os.Stat(ios.KeysPath()); err == nil && file.Mode().IsRegular() {
+		ios.Keys, err = torKeys(ios.KeysPath())
+		if err != nil {
+			return nil, err
+		}
+	}
 	return ios, nil
+}
+
+func torKeys(addr string) (crypto.PrivateKey, error) {
+	//log.Infof("Starting and registering onion service, please wait a couple of minutes...")
+	//t, err := tor.Start(nil, nil)
+	//if err != nil {
+	//	log.Fatalf("Unable to start Tor: %v", err)
+	//}
+	var keys *ed25519.PrivateKey
+	if _, err := os.Stat(addr + ".tor.private"); os.IsNotExist(err) {
+		_, tkeys, err := ed25519.GenerateKey(nil)
+		if err != nil {
+			log.Fatalf("Unable to generate onion service key, %s", err)
+		}
+		keys = &tkeys
+		f, err := os.Create(addr + ".tor.private")
+		if err != nil {
+			log.Fatalf("Unable to create Tor keys file for writing, %s", err)
+		}
+		defer f.Close()
+		_, err = f.Write(tkeys)
+		if err != nil {
+			log.Fatalf("Unable to write Tor keys to disk, %s", err)
+		}
+	} else if err == nil {
+		tkeys, err := ioutil.ReadFile(addr + ".tor.private")
+		if err != nil {
+			log.Fatalf("Unable to read Tor keys from disk")
+		}
+		k := ed25519.NewKeyFromSeed(tkeys)
+		keys = &k
+	} else {
+		log.Fatalf("Unable to set up Tor keys, %s", err)
+	}
+	return keys, nil
 }
 
 func (ios *I2POnionService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -63,21 +108,54 @@ func (ios *I2POnionService) Listen(net, addr string) (net.Listener, error) {
 		return ios.OnionService, nil
 	}
 	fmt.Println("Starting and registering onion service, please wait a couple of minutes...")
-	tb, err := tor.Start(context.Background(), nil)
+	t, err := tor.Start(nil, nil)
 	if err != nil {
-		log.Panicf("Unable to start Tor: %v", err)
+		return nil, fmt.Errorf("Unable to start Tor: %v", err)
 	}
-	//defer tb.Close()
-	//Wait at most a few minutes to publish the service
-	//listenCtx, listenCancel
-	listenCtx := context.Background() //context.WithTimeout(context.Background(), 3*time.Minute)
-	//defer listenCancel()
-	//Create a v3 onion service to listen on any port but show as 80
-	ios.OnionService, err = tb.Listen(listenCtx, &tor.ListenConf{Version3: true, RemotePorts: []int{80}})
+	//var err error
+	listenCtx := context.Background()
+	// Create a v3 onion service to listen on any port but show as 6667
+	ios.OnionService, err = t.Listen(
+		listenCtx,
+		&tor.ListenConf{
+			Version3:    true,
+			RemotePorts: []int{80},
+			Key:         ios.Keys,
+		},
+	)
 	if err != nil {
-		log.Panicf("Unable to create onion service: %v", err)
+		return nil, fmt.Errorf("Unable to listen on Tor: %v", err)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("Unable to write Tor public key to disk, %s", err)
 	}
 	return ios.OnionService, nil
+}
+
+func (ios *I2POnionService) KeysPath() string {
+	return filepath.Join(filepath.Dir(filepath.Dir(ios.ServeDir)), "service.tor.private")
+}
+
+func GenerateTorKeys(file string) (*ed25519.PrivateKey, error) {
+	var keys *ed25519.PrivateKey
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		//tkeys
+		_, tkeys, err := ed25519.GenerateKey(nil)
+		if err != nil {
+			log.Fatalf("Unable to generate onion service key, %s", err)
+		}
+		keys = &tkeys
+		f, err := os.Create(file)
+		if err != nil {
+			log.Fatalf("Unable to create Tor keys file for writing, %s", err)
+		}
+		defer f.Close()
+		_, err = f.Write(tkeys)
+		if err != nil {
+			log.Fatalf("Unable to write Tor keys to disk, %s", err)
+		}
+	}
+	return keys, nil
 }
 
 func (ios *I2POnionService) Serve(l net.Listener) error {
@@ -105,7 +183,6 @@ func (ios *I2POnionService) UnpackSite() error {
 	os.MkdirAll(docroot, 0755)
 	//unpack the contents to the docroot
 	return fs.WalkDir(content, ".", func(embedpath string, d fs.DirEntry, err error) error {
-
 		fp := filepath.Join(docroot)
 		if err != nil {
 			log.Fatal(err)
