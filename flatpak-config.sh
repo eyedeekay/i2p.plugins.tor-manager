b9fcefff0394fe1748e514330b332559cf6b2968
idk <hankhill19580@gmail.com>
2022-07-05 22:40:59 -0400

    bunch of cool flatpak revisions

--- flatpak-config.sh
+++ flatpak-config.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env sh
-TOR_MANAGER_CLEARNET_MIRROR=true TOR_MANAGER_REQUIRE_PASSWORD=false /app/bin/i2p.plugins.tor-manager -p2p=false -config -directory ~/.var/app/org.i2pgit.idk.i2p.plugins.tor-manager
+TOR_MANAGER_CLEARNET_MIRROR=true TOR_MANAGER_REQUIRE_PASSWORD=false /app/bin/i2p.plugins.tor-manager -p2p=false -i2pconfig -directory ~/.var/app/org.i2pgit.idk.i2p.plugins.tor-manager
